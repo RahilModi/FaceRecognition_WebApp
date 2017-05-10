2e1b3e1f4204621437d81d262cffe02d6b318b73
Dhrumil1808 <dhrml.shah@gmail.com>
2017-05-10 16:34:32 -0700

    disabled caching

--- app_basic.py
+++ app_basic.py
@@ -33,6 +33,18 @@ app = Flask(__name__,static_url_path='/static', template_folder="templates")
 app.secret_key = 'dsjksdh88989djj'
 
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.route("/",methods=["GET"])
 def defaultPage():
     print "Arrived in index - ROOT"
